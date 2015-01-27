a1a741a9cc9883befdfc6f05c340bf57a717f5bf
Nimish Mehta <mehta.nimish42@gmail.com>
2015-01-28 02:05:43 +0530

    DELETE/PUT working

--- api/invoices/views.py
+++ api/invoices/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, status
 from django.db import transaction as transaction_control
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import Http404
 
 
 class InvoiceList(APIView):
@@ -18,11 +19,11 @@ class InvoiceList(APIView):
         if serializer.is_valid():
             invoice = self.save_invoice(data)
             response_serializer = InvoiceSerializer(invoice)
-            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(response_serializer.data,
+                            status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def save_invoice(self, data):
-        print data
         transaction_list = data.get('transactions')
         customer = data.get('customer')
         with transaction_control.atomic():
@@ -40,7 +41,45 @@ class InvoiceList(APIView):
         return inv
 
 
+class InvoiceDetail(APIView):
 
-class InvoiceDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Invoice.objects.all()
-    serializer_class = InvoiceSerializer
+    def get_object(self, pk):
+        try:
+            return Invoice.objects.get(pk=pk)
+        except Invoice.DoesNotExist:
+            raise Http404
+
+    def update_invoice(self, data):
+        transaction_list = data.get('transactions')
+        with transaction_control.atomic():
+            inv = self.get_object(data.get('id'))
+            for t in transaction_list:
+                print t
+                id = t.get('id')
+                transact = Transaction(pk=id)
+                transact.price = t.get('price')
+                transact.product = t.get('product')
+                transact.quantity = t.get('quantity')
+                transact.invoice = inv
+                transact.save()
+            inv.save()
+        return inv
+
+    def get(self, request, pk, format=None):
+        invoice = self.get_object(pk)
+        serializer = InvoiceSerializer(invoice)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        invoice = self.get_object(pk)
+        invoice.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk, format=None):
+        invoice = self.get_object(pk)
+        serializer = InvoiceSerializer(invoice, data=request.data)
+        if serializer.is_valid():
+            invoice = self.update_invoice(request.data)
+            response_serializer = InvoiceSerializer(invoice)
+            return Response(response_serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
